9666c81afa7ed4873469e9b7fbad568c1af11039
MafiaAlien <49702823+MafiaAlien@users.noreply.github.com>
2022-09-23 17:12:37 -0700

    "second push"

--- main.go
+++ main.go
@@ -1,6 +1,36 @@
 package main
 
+import (
+	"net/http"
+	"os"
+	// "io"
+	"fmt"
+)
 
 func main(){
+	links := []string {
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	for _, link := range links {
+			go chekcLink(link)
+		}
+		
+	
+	}
+
+func chekcLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link + " might be down!")
+		os.Exit(1)
+		return 
+	}
+
+	fmt.Println(link, " is up!")
 
 }
